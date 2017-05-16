2ba5b36940b7f006aad9ce2681f52abaf74a40e8
Jairo Junior <jairo.silvadeaguiar@gmail.com>
2017-05-16 12:26:56 -0300

    Add files via upload

---
+++ titanic.py
@@ -0,0 +1,124 @@
+# Imports
+
+# pandas
+import pandas as pd
+from pandas import Series,DataFrame
+
+# numpy, matplotlib, seaborn
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set_style('whitegrid')
+
+# machine learning
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC, LinearSVC
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB
+
+# get titanic & test csv files as a DataFrame
+titanic_df = pd.read_csv("train.csv")
+test_df    = pd.read_csv("test.csv")
+
+# drop unnecessary columns, these columns won't be useful in analysis and prediction
+titanic_df = titanic_df.drop(['PassengerId','Name','Ticket'], axis=1)
+test_df    = test_df.drop(['PassengerId','Name','Ticket'], axis=1)
+
+# Embarked
+titanic_df.drop(['Embarked'], axis=1,inplace=True)
+test_df.drop(['Embarked'], axis=1,inplace=True)
+
+# Fare
+# only for test_df, since there is a missing "Fare" values
+test_df["Fare"].fillna(test_df["Fare"].median(), inplace=True)
+# convert from float to int
+titanic_df['Fare'] = titanic_df['Fare'].astype(int)
+test_df['Fare']    = test_df['Fare'].astype(int)
+
+# Age 
+# get average, std, and number of NaN values in titanic_df
+average_age_titanic   = titanic_df["Age"].mean()
+std_age_titanic       = titanic_df["Age"].std()
+count_nan_age_titanic = titanic_df["Age"].isnull().sum()
+
+# get average, std, and number of NaN values in test_df
+average_age_test   = test_df["Age"].mean()
+std_age_test       = test_df["Age"].std()
+count_nan_age_test = test_df["Age"].isnull().sum()
+
+# generate random numbers between (mean - std) & (mean + std)
+rand_1 = np.random.randint(average_age_titanic - std_age_titanic, average_age_titanic + std_age_titanic, size = count_nan_age_titanic)
+rand_2 = np.random.randint(average_age_test - std_age_test, average_age_test + std_age_test, size = count_nan_age_test)
+
+# fill NaN values in Age column with random values generated
+titanic_df["Age"][np.isnan(titanic_df["Age"])] = rand_1
+test_df["Age"][np.isnan(test_df["Age"])] = rand_2
+
+# convert from float to int
+titanic_df['Age'] = titanic_df['Age'].astype(int)
+test_df['Age']    = test_df['Age'].astype(int)
+
+# Cabin
+# It has a lot of NaN values, so it won't cause a remarkable impact on prediction
+titanic_df.drop("Cabin",axis=1,inplace=True)
+test_df.drop("Cabin",axis=1,inplace=True)
+
+# Family
+# Instead of having two columns Parch & SibSp, 
+# we can have only one column represent if the passenger had any family member aboard or not,
+# Meaning, if having any family member(whether parent, brother, ...etc) will increase chances of Survival or not.
+titanic_df['Family'] =  titanic_df["Parch"] + titanic_df["SibSp"]
+titanic_df['Family'].loc[titanic_df['Family'] > 0] = 1
+titanic_df['Family'].loc[titanic_df['Family'] == 0] = 0
+
+test_df['Family'] =  test_df["Parch"] + test_df["SibSp"]
+test_df['Family'].loc[test_df['Family'] > 0] = 1
+test_df['Family'].loc[test_df['Family'] == 0] = 0
+
+# drop Parch & SibSp
+titanic_df = titanic_df.drop(['SibSp','Parch'], axis=1)
+test_df    = test_df.drop(['SibSp','Parch'], axis=1)
+
+# Sex
+
+# As we see, children(age < ~16) on aboard seem to have a high chances for Survival.
+# So, we can classify passengers as males, females, and child
+def get_person(passenger):
+    age,sex = passenger
+    return 'child' if age < 16 else sex
+    
+titanic_df['Person'] = titanic_df[['Age','Sex']].apply(get_person,axis=1)
+test_df['Person']    = test_df[['Age','Sex']].apply(get_person,axis=1)
+
+# No need to use Sex column since we created Person column
+titanic_df.drop(['Sex'],axis=1,inplace=True)
+test_df.drop(['Sex'],axis=1,inplace=True)
+
+person_dummies_titanic  = pd.get_dummies(titanic_df['Person'])
+person_dummies_titanic.columns = ['Child','Female','Male']
+
+person_dummies_test  = pd.get_dummies(test_df['Person'])
+person_dummies_test.columns = ['Child','Female','Male']
+
+titanic_df = titanic_df.join(person_dummies_titanic)
+test_df    = test_df.join(person_dummies_test)
+
+titanic_df.drop(['Person'],axis=1,inplace=True)
+test_df.drop(['Person'],axis=1,inplace=True)
+
+X_train = titanic_df.drop("Survived",axis=1)
+Y_train = titanic_df["Survived"]
+X_test  = test_df.copy()
+
+# Random Forests
+
+random_forest = RandomForestClassifier(n_estimators=100)
+
+random_forest.fit(X_train, Y_train)
+
+Y_pred = random_forest.predict(X_test)
+
+print(random_forest.score(X_train, Y_train))
+
+
